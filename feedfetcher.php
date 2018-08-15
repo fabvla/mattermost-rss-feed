5828ff366d2bd2ceaa832d767514afdc9c584849
Denis Torresan <denis.torresan@fabvla.com>
2018-08-15 17:25:01 +0200

    first commit

---
+++ feedfetcher.php
@@ -0,0 +1,51 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/config.php';
+
+use GuzzleHttp\Client;
+use ThibaudDauce\Mattermost\Mattermost;
+use ThibaudDauce\Mattermost\Message;
+use ThibaudDauce\Mattermost\Attachment;
+
+$last_run = mktime(0, 0, 0, 1, 1, 1970);
+if( file_exists("last_run") ){
+	$last_run = filemtime("last_run");
+}
+
+echo "Fetch RSS $RSS_URL \n";
+
+$rss = Feed::loadRss($RSS_URL);
+
+$mattermost = new Mattermost(new Client);
+
+//reverse items
+$rss_items = array();
+foreach ($rss->item as $item) {
+	$rss_items[] = $item;
+}
+$rss_items = array_reverse($rss_items);
+
+//post
+$new_post_count = 0;
+foreach ($rss_items as $item) {
+	
+	if( $item->timestamp > $last_run ) {
+		$message = (new Message)
+			->channel($MATTERMOST_CHANNEL)
+			->username($MATTERMOST_USERNAME)
+			->iconUrl($RSS_IMAGE_URL)
+			->text(''.$item->link);
+		
+		echo "POST $item->title \n";
+		
+		$mattermost->send($message, $MATTERMOST_WEBHOOK);
+		
+		$new_post_count++;
+	}
+
+}
+
+echo "New posts added: $new_post_count\n";
+
+touch("last_run");
